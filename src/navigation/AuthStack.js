318328041eb2fd5a1b693b2caccf1e280212494c
A7abhilash <a7abhilash@gmail.com>
2021-05-01 15:40:40 +0530

    added logout to header

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import AuthDrawer from './AuthDrawer';
+import {Text, TouchableOpacity} from 'react-native';
+import {useAuth} from '../contexts/AuthContext';
 
 export default function AuthStack() {
   const Stack = createStackNavigator();
@@ -9,8 +11,23 @@ export default function AuthStack() {
       headerMode="screen"
       screenOptions={{
         headerTitle: 'Get Stuff Done',
+        headerRight: () => <Logout />,
       }}>
       <Stack.Screen name="AuthDrawer" component={AuthDrawer} />
     </Stack.Navigator>
   );
 }
+
+const Logout = () => {
+  const {logout} = useAuth();
+
+  return (
+    <TouchableOpacity onPress={logout} style={{marginRight: 10}}>
+      {/* <Image
+        source={require("./../assets/icons/logout.png")}
+        style={{ width: 24, height: 24, resizeMode: "center" }}
+      /> */}
+      <Text>Logout</Text>
+    </TouchableOpacity>
+  );
+};
