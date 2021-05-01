48b046b9b2fc73a0b0c7f32b5dfa3a9a9d17875f
A7abhilash <a7abhilash@gmail.com>
2021-05-01 16:16:35 +0530

    updated drawer UI

--- src/navigation/AuthDrawer.js
+++ src/navigation/AuthDrawer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import Todos from '../authScreens/Todos';
+import {globalColors} from '../styles/styles';
 
 export default function AuthDrawer() {
   const Drawer = createDrawerNavigator();
@@ -8,9 +9,24 @@ export default function AuthDrawer() {
   const times = ['Today', 'This Week', 'Later', 'Pending', 'Completed'];
 
   return (
-    <Drawer.Navigator drawerPosition="left">
+    <Drawer.Navigator
+      drawerStyle={{
+        backgroundColor: globalColors.Gray,
+      }}
+      drawerContentOptions={{
+        activeTintColor: globalColors.Warning,
+        inactiveTintColor: globalColors.Light,
+        activeBackgroundColor: globalColors.Secondary,
+      }}>
       {times.map(time => (
-        <Drawer.Screen name={time} key={time}>
+        <Drawer.Screen
+          name={time}
+          key={time}
+          options={{
+            headerStyle: {
+              marginVertical: 0,
+            },
+          }}>
           {props => <Todos {...props} type={time} />}
         </Drawer.Screen>
       ))}
